8137ad0a5536e03ccba1225b6a61cd951a232870
Ayaan Merchant <70717456+AyaanMerchant@users.noreply.github.com>
2025-08-05 04:11:02 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,16 +9,15 @@ st.write(
     """Choose the fruits you want in your custom Smoothie!
     """
 )
-name_on_order = st.text_input('Name on Smothie: ')
+
+name_on_order = st.text_input('Name on Smoothie: ')
 st.write('The name on your Smoothie will be: ', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-
 my_dataframe = session.table(
     "smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 pd_df = my_dataframe.to_pandas()
-
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients",
@@ -27,27 +26,25 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
-    
     ingredients_string = ''
-
+    
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-
+    
     st.write(ingredients_string)
-
+    
     my_insert_stmt = """ insert into smoothies.public.orders
     (ingredients, name_on_order)
     values ('""" + ingredients_string + """', '""" + name_on_order +"""')"""
-
+    
     time_to_insert = st.button('Submit Order')
-
+    
     if time_to_insert:
-        
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
